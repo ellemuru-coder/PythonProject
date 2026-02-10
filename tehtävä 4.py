5cfc6750bbdd696650e41f62edaf98f5c2f25e16
ellemuru-coder <ellemuru@gmail.com>
2026-02-10 16:40:32 +0200

    Add files via upload

---
+++ tehtävä 4.py
@@ -0,0 +1,9 @@
+a = int(input("Anna ensimmäinen kokonaisluku: "))
+b = int(input("Anna toinen kokonaisluku: "))
+c = int(input("Anna kolmas kokonaisluku: "))
+summa = a + b + c
+tulo = a * b * c
+keskiarvo = summa / 3
+print("Summa:", summa)
+print("Tulo:", tulo)
+print("Keskiarvo:", keskiarvo)
